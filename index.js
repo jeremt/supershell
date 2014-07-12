52d5a971b09adb4a9a3a5db532baaf3f4e949433
Jeremie Taboada <taboada.jeremie@gmail.com>
2014-07-12 22:28:08 +0200

    Update package.json to have a valid module.

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 
-var command = require('./lib/command');
-
-module.exports = {
-  exec: command.exec,
-  Command: command.Command,
-  config: require('./lib/config'),
-  parsers: require('./lib/parsers'),
-  log: require('./lib/log')
-};
+module.exports = require('./lib/supershell')
